27bbe6615ee9075a83e5a80d793016e1eaa54fac
dylan hokstam <dylan@hokstam.com>
2025-03-06 21:32:28 +0100

    updated the main page and created 2 more draggable items.

---
+++ scripts/moveDiv/moveDiv4.js
@@ -0,0 +1,62 @@
+function randomizePosition(elmnt) {
+    const viewportWidth = window.innerWidth;
+    const viewportHeight = window.innerHeight;
+
+    const randomX = Math.floor(Math.random() * (viewportWidth - elmnt.offsetWidth));
+    const randomY = Math.floor(Math.random() * (viewportHeight - elmnt.offsetHeight));
+
+    elmnt.style.position = "absolute";
+    elmnt.style.left = 0 + "%";
+    elmnt.style.top = 9 + "%";
+
+}
+
+const mtxt = document.getElementById("mainPageText");
+randomizePosition(mtxt);
+dragElement(mtxt);
+
+
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, initialX = 0, initialY = 0;
+
+    if (document.getElementById(elmnt.id + "windowHeader4")) {
+        document.getElementById(elmnt.id + "windowHeader4").onmousedown = dragMouseDown;
+    } else {
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e.preventDefault();
+
+        mtxt.style.cursor = "grabbing";
+
+        // Get initial cursor position and element position:
+        initialX = e.clientX;
+        initialY = e.clientY;
+        pos1 = elmnt.offsetLeft;
+        pos2 = elmnt.offsetTop;
+
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e.preventDefault();
+
+        // Calculate new position:
+        let deltaX = e.clientX - initialX;
+        let deltaY = e.clientY - initialY;
+
+        // Set the element's new position:
+        elmnt.style.left = (pos1 + deltaX) + "px";
+        elmnt.style.top = (pos2 + deltaY) + "px";
+    }
+
+    function closeDragElement() {
+        mtxt.style.cursor = "grab";
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+
